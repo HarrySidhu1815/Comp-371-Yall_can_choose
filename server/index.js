ff043678f247122b7ef0d9185a22da92a34a8412
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-06-10 11:54:59 -0700

    database uri changed

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use(session({
 }));
 
 
-mongoose.connect('mongodb://localhost:27017/', {
+mongoose.connect(process.env.MONGODB_URI, {
   })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
