6d2c8d333c580fb7e10688127528baf8227e0a84
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-06-09 22:32:28 -0700

    rollup plugin added

--- UniFlipp/vite.config.js
+++ UniFlipp/vite.config.js
@@ -1,21 +1,24 @@
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     nodePolyfills(),
-    react()
+    react(),
+    visualizer({
+      filename: './dist/stats.html',
+      open: true,
+    })
   ],
   build: {
-    chunkSizeWarningLimit: 500, // Set limit to 1000 kB
+    chunkSizeWarningLimit: 1000, // Increased limit to 1000 kB
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+        manualChunks: {
+          lodash: ['lodash']
         }
       }
     }
